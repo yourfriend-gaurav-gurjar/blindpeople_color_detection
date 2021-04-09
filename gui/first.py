7742c286474eeb1de39fdb7d1e3fef8e371ffcfe
yourfriend-gaurav-gurjar <gaurav@metteyyaanalyticsllc.com>
2021-04-09 06:17:05 -0400

    gui for camera click added.

--- gui/first.py
+++ gui/first.py
@@ -1,5 +1,23 @@
 #! /usr/bin/python
-import Tkinter as tk
+import os.path
+from tkinter import *
+from PIL import ImageTk, Image
+from picamera import PiCamera
+from time import sleep
 
-top = tk()
-top.mainloop()
+root = Tk()
+root.title("Color Blindness Project")
+
+# Capture Image from the camera
+def capPic():
+    camera=PiCamera()
+    camera.start_preview()
+    sleep(5)
+    camera.capture("pic.jpg")
+    camera.stop_preview()
+
+
+button_cap = Button(root, text= "Capture Photo", command=capPic)
+button_cap.pack()
+
+root.mainloop()
